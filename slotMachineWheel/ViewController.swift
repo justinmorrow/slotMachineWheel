fb724f430130d773525a666ea92da90071899079
Justin Morrow <justin.morrow81@gmail.com>
2015-12-25 22:38:04 -0500

    Initial Commit

---
+++ slotMachineWheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slotMachineWheel
+//
+//  Created by Justin Morrow on 12/25/15.
+//  Copyright © 2015 Justin Morrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
