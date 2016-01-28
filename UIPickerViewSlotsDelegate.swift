3b028bca5d73c960dab22e124f154f657294f150
Justin Morrow <justin.morrow81@gmail.com>
2016-01-28 14:04:41 -0500

    Initial work on project

---
+++ UIPickerViewSlotsDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  UIPickerViewDelegateSlots.swift
+//  slotMachineWheel
+//
+//  Created by Justin Morrow on 12/26/15.
+//  Copyright © 2015 Justin Morrow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIPickerViewSlotsDelegate : UIPickerViewDelegate {
+    let slotHeight = 15;
+    let slotWidth = 40;
+    
+    
+    
+}
